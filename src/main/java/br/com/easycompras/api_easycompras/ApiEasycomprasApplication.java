9d4433b66bb53a2dc9ad4a5b8ffabf88dc8c04f5
igoorlemoos <igor.x41@hotmail.com>
2023-10-21 11:12:18 -0300

    Arquivos iniciais api

---
+++ src/main/java/br/com/easycompras/api_easycompras/ApiEasycomprasApplication.java
@@ -0,0 +1,13 @@
+package br.com.easycompras.api_easycompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEasycomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEasycomprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/easycompras/api_easycompras/ApiEasycomprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easycompras.api_easycompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEasycomprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
